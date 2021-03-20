ac4d7cf9b11eb0310e736ba2df9ccc89798f7673
Domenico <2201547R@student.gla.ac.uk>
2021-03-20 16:05:53 +0000

    Temp detector classes

--- tsic/main.cpp
+++ tsic/main.cpp
@@ -10,15 +10,22 @@ using namespace std;
 
 
 int main(){
+    cout << "Initializing";
+
     TSIC* detector1 = new TSIC();
     detector1->open(14);
+    cout << "Detector opened";
 
     double detector1Temp= 0.0;
 
     while (1){
+        cout << "1 Sec passed";
+
         bool received = detector1->getDegrees(detector1Temp);
         if (received){
             cout << detector1Temp;
+        } else {
+            cout << "Still invalid";
         }
         sleep(1);
     }
