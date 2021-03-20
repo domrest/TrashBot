5b672df9b29c5caa8fc9a0ab1997f1c2736ade1c
Domenico <2201547R@student.gla.ac.uk>
2021-03-20 15:37:22 +0000

    Temp detector classes

---
+++ tsic/main.cpp
@@ -0,0 +1,22 @@
+//
+// Created by domrest on 20/03/2021.
+//
+
+#include "tsic.h"
+#include <iostream>
+
+
+int main(){
+    TSIC detector1 = new TSIC();
+    detector1.open(14);
+
+    double detector1Temp= 0.0;
+
+    while (1){
+        bool received = detector1.getDegrees(&detector1Temp);
+        if (received){
+            cout << detector1Temp;
+        }
+        sleep(1);
+    }
+}
