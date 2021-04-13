f5e1cb68d57a605c8c359091f67c65b3ada0ec2a
Smithereens101 <77978118+Smithereens101@users.noreply.github.com>
2021-04-13 14:44:33 +0100

    Update Messaging.h
    Cleaned up

--- include/messaging/Messaging.h
+++ include/messaging/Messaging.h
@@ -5,37 +5,29 @@
 
 class INFO{
 
-public: //this is the default, public has to be specified
+public:
 
-	std::string username;
-    std::string password;
-    std::string serverAddress;
-	int port;
-    std::string sender;
+	std::string username		;
+    	std::string password		;
+    	std::string serverAddress	;
+	int port 			;
+    	std::string sender 		;
 
-	//SMTP Messaging::MySMTP;
-
-    std::string destination; //to
-    std::string subject;	    //subject
-    std::string message1;	//main body;
-    std::string message2;
-    std::string startup;
+    	std::string destination		; //to
+    	std::string subject		; //subject
+    	std::string message1		; //main body
+    	std::string message2		;
+    	std::string startup		;
 
 };
 
 class Messaging {
-
 public:
 	Messaging();
-	// class Car: public Vehicle{} is called inheritance
-
-	/// Destructor
-	//~Messaging();
-
 	INFO info;
 
 	void startup();
-    void sendMessage1();
-    void sendMessage2();
+    	void sendMessage1();
+    	void sendMessage2();
 
 };
